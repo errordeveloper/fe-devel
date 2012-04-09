f347234027fd1707909f0852ead8f78208105a31
Peter Zion <peter.zion@gmail.com>
2012-04-09 17:11:02 -0400

    Add support for FABRIC_EXT_PATH environment variable

--- Native/Clients/Node/Client.cpp
+++ Native/Clients/Node/Client.cpp
@@ -4,6 +4,7 @@
 
 #include <Fabric/EDK/EDK.h>
 #include <Fabric/Clients/Node/Client.h>
+#include <Fabric/Core/Plug/Helpers.h>
 #include <Fabric/Core/DG/Context.h>
 #include <Fabric/Base/JSON/Encoder.h>
 #include <Fabric/Core/IO/Helpers.h>
@@ -211,30 +212,8 @@ namespace Fabric
       : m_mutex("Node.js ClientWrap")
     {
       std::vector<std::string> pluginPaths;
-#if defined(FABRIC_OS_MACOSX)
-      char const *home = getenv("HOME");
-      if ( home && *home )
-      {
-        std::string homePath( home );
-        pluginPaths.push_back( IO::JoinPath( homePath, "Library", "Fabric", "Exts" ) );
-      }
-      pluginPaths.push_back( "/Library/Fabric/Exts" );
-#elif defined(FABRIC_OS_LINUX)
-      char const *home = getenv("HOME");
-      if ( home && *home )
-      {
-        std::string homePath( home );
-        pluginPaths.push_back( IO::JoinPath( homePath, ".fabric", "Exts" ) );
-      }
-      pluginPaths.push_back( "/usr/lib/fabric/Exts" );
-#elif defined(FABRIC_OS_WINDOWS)
-      char const *appData = getenv("APPDATA");
-      if ( appData && *appData )
-      {
-        std::string appDataDir(appData);
-        pluginPaths.push_back( IO::JoinPath( appDataDir, "Fabric" , "Exts" ) );
-      }
-#endif
+      Plug::AppendUserPaths( pluginPaths );
+      Plug::AppendGlobalPaths( pluginPaths );
 
       CG::CompileOptions compileOptions;
       if ( compileGuarded > -1 )
